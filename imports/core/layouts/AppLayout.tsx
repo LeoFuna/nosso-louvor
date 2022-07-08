37acac8cd7f5d25e828a468b63ecaf73ad51593f
Leo "Funa" Funabashi <leo.funa@gmail.com>
2022-07-08 08:35:46 -0300

    feat ( AppLayout ): app layout component (#2)
    * styles: lint adjusts
    
    * feat: Head of AppLayout
    
    * feat: using AppLayout on dashboard scale
    
    * feat: Avatar with logo on AppLayout
    
    * styles: create theme
    
    * chore: material ui icons and babel
    
    * feat: setting path for imports and enviroments on babel
    
    * feat: appLayout core
    
    * feat: scale page initial

---
+++ imports/core/layouts/AppLayout.tsx
@@ -0,0 +1,72 @@
+import { AppBar, Toolbar, Typography, IconButton, Avatar, Box } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+import { Menu } from "@material-ui/icons";
+import Head from "next/head";
+import { ReactNode } from "react";
+
+interface AppLayoutInterface {
+  title: string,
+  children: ReactNode,
+  disableHamburguer?: boolean,
+}
+
+const useStyles = makeStyles({
+  appBar: {
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid',
+  },
+  toolBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  stdMargin: {
+    margin: '4px',
+  },
+});
+
+function AppLayout({
+  title,
+  children,
+  disableHamburguer = false,
+}: AppLayoutInterface) {
+  const classes = useStyles();
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <AppBar className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <Box className={classes.toolBar}>
+           { !disableHamburguer &&
+            <IconButton className={classes.stdMargin}>
+              <Menu />
+            </IconButton> }
+            <Avatar className={classes.stdMargin} variant='rounded' src='/ipbl-logo.png' />
+            <Typography className={classes.stdMargin} variant='h5'>
+              {title}
+            </Typography>
+          </Box>
+          <Box className={classes.toolBar}>
+            <Box className={classes.stdMargin}>
+              {'< Junho >'}
+            </Box>
+            <div className={classes.stdMargin}>
+              <Avatar>
+                LF
+              </Avatar>
+            </div>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box pl={3} pr={3}>
+          {children}
+        </Box>
+      </main>
+    </>
+  );
+}
+
+export default AppLayout;
---
+++ imports/core/layouts/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+import { ptBR } from "@material-ui/core/locale";
+
+export default createTheme({
+  palette: {
+    secondary: {
+      light: '#fa3f43',
+      main: '#f90f14',
+      dark: '#ae0a0e',
+    },
+  },
+}, ptBR);
