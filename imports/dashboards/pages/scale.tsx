576fa604318fbb2cec37ad9925387fd7c3b9ca4f
leofuna <leo.funa@gmail.com>
2022-07-08 08:34:30 -0300

    feat: scale page initial

---
+++ imports/dashboards/pages/scale.tsx
@@ -0,0 +1,13 @@
+import AppLayout from '../../core/layouts/AppLayout';
+
+function DashboardScale() {
+  return(
+    <AppLayout disableHamburguer title='Escalas'>
+      <div style={{ height: "400px", backgroundColor: "red" }}>
+        <h3 style={{ marginTop: "200px" }}>TESTE ESCALA</h3>
+      </div>
+    </AppLayout>
+  );
+}
+
+export default DashboardScale;
