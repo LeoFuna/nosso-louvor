729a6cc34b219632b3b388fbcc6978791073a42b
leofuna <leo.funa@gmail.com>
2022-08-03 09:37:13 -0300

    feat: status code utils

---
+++ imports/core/utils/status-code.utils.ts
@@ -0,0 +1,14 @@
+type StatusCode = {
+  CREATED: number,
+  OK: number,
+  NOT_FOUND: number,
+  SERVER_ERROR: number,
+}
+
+export const STATUS_CODE: StatusCode = {
+  CREATED: 201,
+  OK: 200,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+};
+
