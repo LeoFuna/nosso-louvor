94d2fd0afe8a839b4b595709613d192fd11b898b
leofuna <leo.funa@gmail.com>
2022-08-03 09:38:03 -0300

    feat: get and post users

--- imports/users/api/user.api.ts
+++ imports/users/api/user.api.ts
@@ -2,16 +2,45 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import db from '../../../prisma';
 import express from 'express';
+import { STATUS_CODE } from 'imports/core/utils/status-code.utils';
 
-type Data = {
-  allUsers: any
+type UserNoPassword = {
+  id: number;
+  username: string;
+  name: string;
+  lastName: string;
+  roleId: number;
+  active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type Users = {
+  allUsers: UserNoPassword[],
 }
 
 const app = express();
 
-app.get('/api/users', async (req: express.Request, res: express.Response<Data>) => {
+app.get('/api/users', async (_req: express.Request, res: express.Response<Users>) => {
   const allUsers = await db.user.findMany();
-  res.status(200).json({ allUsers });
+  const usersWithoutPassword = allUsers.map((user) => {
+    const { password, ...userDataNoPassword } = user;
+    return userDataNoPassword;
+  });
+  res.status(STATUS_CODE.OK).json({ allUsers: usersWithoutPassword });
+});
+app.post('/api/users', async (req: express.Request, res: express.Response<UserNoPassword>) => {
+  const { username, password, name, lastName, roleId } = req.body;
+  const createResponse = await db.user.create({
+    data: {
+      username,
+      password,
+      name,
+      lastName,
+      roleId,
+    },
+  });
+  res.status(STATUS_CODE.CREATED).json(createResponse);
 });
 
 export default app;
