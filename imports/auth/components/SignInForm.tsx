e24c74fb9727297876c7e1d97db8ed5e438df569
leofuna <leo.funa@gmail.com>
2022-08-04 09:02:25 -0300

    feat: sign in form

---
+++ imports/auth/components/SignInForm.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, InputLabel, TextField } from '@mui/material';
+import { signIn } from 'next-auth/react';
+import { Controller, useForm } from 'react-hook-form';
+import Image from 'next/image';
+
+type LoginFormValues = {
+  username: string,
+  password: string,
+}
+
+const SignInForm = () => {
+  const { control, handleSubmit } = useForm<LoginFormValues>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  return (
+    <Box width='100%'>
+      <Box textAlign='center' mb='24px'>
+        <Image alt='ipbl logo' src='/ipbl-logo.png' width='250' height='250' style={{borderRadius: '10%'}} />
+      </Box>
+      <form>
+        <Box mb={2} mt={2} display='flex' alignItems='center'>
+          <Box mr={1} width='80px'>
+            <InputLabel>Usuário</InputLabel>
+          </Box>
+          <Controller 
+            name='username'
+            control={control}
+            render={({field}) => (
+              <TextField  {...field} fullWidth variant='standard' />
+            )}
+          />
+        </Box>
+        <Box mb={2} display='flex' alignItems='center'>
+          <Box mr={1} width='80px'>
+            <InputLabel>Senha</InputLabel>
+          </Box>
+          <Controller 
+            name='password'
+            control={control}
+            render={({field}) => (
+              <TextField {...field} fullWidth type='password' variant='standard' />
+            )}
+          />
+        </Box>
+        <Box textAlign='right'>
+          <Button type='submit'>
+            Entrar
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  );
+};
+
+export default SignInForm;
