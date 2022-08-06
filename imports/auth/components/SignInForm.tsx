28e80b36db3a9a8a5def1620d1494b79de15697f
leofuna <leo.funa@gmail.com>
2022-08-06 10:18:42 -0300

    feat: firebase

--- imports/auth/components/SignInForm.tsx
+++ imports/auth/components/SignInForm.tsx
@@ -1,7 +1,10 @@
 import { Box, Button, InputLabel, TextField } from '@mui/material';
 import { signIn } from 'next-auth/react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from 'imports/core/services/firebase';
 import { Controller, useForm } from 'react-hook-form';
 import Image from 'next/image';
+import { GoogleLogo } from 'phosphor-react';
 
 type LoginFormValues = {
   username: string,
@@ -16,12 +19,23 @@ const SignInForm = () => {
     },
   });
 
+  function handleGoogleSignIn() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e)
+      });
+  }
+
   return (
     <Box width='100%'>
       <Box textAlign='center' mb='24px'>
         <Image alt='ipbl logo' src='/ipbl-logo.png' width='250' height='250' style={{borderRadius: '10%'}} />
       </Box>
-      <form>
+      <form onSubmit={() => console.log('meu formulário')}>
         <Box mb={2} mt={2} display='flex' alignItems='center'>
           <Box mr={1} width='80px'>
             <InputLabel>Usuário</InputLabel>
@@ -47,8 +61,14 @@ const SignInForm = () => {
           />
         </Box>
         <Box textAlign='right'>
-          <Button type='submit'>
+          <Button onClick={() => console.log('submit com usuario e senha')}>
             Entrar
+          </Button>
+        </Box>
+        <Box textAlign='right'>
+          <Button onClick={handleGoogleSignIn} style={{ backgroundColor: '#046909', color: 'snow' }}>
+            <GoogleLogo style={{ marginRight: '7px', color: 'red', fontSize: '20px' }} />
+            Entrar com Google
           </Button>
         </Box>
       </form>
